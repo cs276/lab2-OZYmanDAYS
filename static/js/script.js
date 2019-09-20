a5d45a1f40efe8dda987bc367e051dead5e5226d
OZYmanDAYS <adewuyi2017@yahoo.com>
2019-09-20 14:43:34 -0500

    starting from our old code

--- static/js/script.js
+++ static/js/script.js
@@ -1,188 +1,172 @@
-const API_KEY = 'API KEY';
+const API_KEY = "8b42a0c0-d56a-11e9-86e9-6733b6ff9504";
 const url = `https://api.harvardartmuseums.org/gallery?apikey=${API_KEY}`;
 
-const allGalleries = document.querySelector('#all-galleries');
-const galleriesContainer = document.querySelector('#galleries');
-const allObjects = document.querySelector('#all-objects');
-const objectDetails = document.querySelector('#object-details');
+const galleries = document.querySelector("#galleries");
+const allObjects = document.querySelector("#all-objects");
+const allGalleries = document.querySelector("#all-galleries");
+const objects = document.querySelector("#objects");
+const objectview = document.querySelector("#object-view");
+const objectinfo = document.querySelector("#objectinfo");
+const button = document.querySelector("#gobackbutton");
+const button1 = document.querySelector("#objecttablebackbutton");
+const button2 = document.querySelector("#objectviewbackbutton");
+const floorOption = document.querySelector("#floorList");
 const objectContainer = document.querySelector('#object');
-const objectsTableBody = document.querySelector('#objects');
+var info = {favs: []};
 
-// Add home buttons
-document.querySelectorAll('.home').forEach((button) => {
-  button.addEventListener('click', () => {
-    // Clear contents of #all-galleries
-    galleriesContainer.innerHTML = '';
-    showGalleriesTable();
-  });
-});
-
-// Get hash link, remove #
-const hash = window.location.hash.slice(1);
-
-if (hash) {
-
-  // List objects from specified gallery
-  showObjectsTable(hash);
-}
-else {
-
-  // List all galleries
-  showGalleriesTable();
-}
-
-function showGalleriesTable() {
-  allObjects.style.display = 'none';
-  objectDetails.style.display = 'none';
-  allGalleries.style.display = 'block';
-  showGalleries(url);
-}
+function load() {
+  let hash = (window.location.hash).replace('#', '');
+  if (hash.length == 0) {
+      showGalleries(url);}
+  }
 
 
-/**
- * Lists all galleries
- */
+window.onload = load;
+
 function showGalleries(url) {
-
-  // Fetch list of galleries
+  allObjects.style.display = "none";
+  allGalleries.style.display = "block";
+  objectview.style.display = "none";
   fetch(url)
-
-  // Convert JSON response to JavaScript object
   .then((response) => response.json())
   .then((data) => {
-    // Iterate over galleries
     data.records.forEach((gallery) => {
-      // We've commented this function a bit more thoroughly to let you see the process of DOM manipualation
-      // in greater detail
-
-      // Create the link
-      const a = document.createElement('a');
-
-      // Set link properties using JavaScript instead of string concatenation
-      a.setAttribute('href', `#${gallery.id}`);
-      a.addEventListener('click', () => {
-        showObjectsTable(gallery.id);
-      });
-
-      // Place the text inside the link
-      a.append(`Gallery #${gallery.id}: ${gallery.name} (Floor ${gallery.floor})`);
-
-      // Add the link to the list item
-      const li = document.createElement('li');
-      li.append(a);
-
-      // Add the list item to the gallery list
-      galleriesContainer.append(li);
+      galleries.innerHTML += `
+        <li class="list-group-item">
+          <a href="#${gallery.id}" onclick="showObjectsTable(${gallery.id})">
+            Gallery #${gallery.id}: ${gallery.name} (Floor ${gallery.floor})
+          </a>
+        </li>
+      `;
     });
 
-    // Fetch and list more galleries if any
     if (data.info.next) {
       showGalleries(data.info.next);
     }
-  });
+  })
 }
 
-
-/**
- * Shows objects table and hides other divs
- */
-function showObjectsTable(id) {
-
-  // Clear contents of #objects
-  objectsTableBody.innerHTML = '';
-
-  // Show #all-objects and hide the others
-  allGalleries.style.display = 'none';
-  objectDetails.style.display = 'none';
-  allObjects.style.display = 'block';
-
-  // List all objects in specified gallery
-  showObjects(`https://api.harvardartmuseums.org/object?apikey=${API_KEY}&gallery=${id}`);
-}
-
-
-/**
- * Lists all objects in a gallery
- */
-function showObjects(url) {
+function showGalleryFloor(url) {
+  allObjects.style.display = "none";
+  allGalleries.style.display = "block";
+  objectview.style.display = "none";
+  let e = document.getElementById("floorList");
+  let floor = e.options[e.selectedIndex].value;
   fetch(url)
   .then((response) => response.json())
   .then((data) => {
-    data.records.forEach((object) => {
-      const tr = document.createElement('tr');
-      const titleCell = document.createElement('td');
-      const titleLink = document.createElement('a');
-      titleLink.setAttribute('href', '#');
-      titleLink.append(object.title);
-      titleLink.addEventListener('click', () => showObjectInfo(object.id));
-      titleCell.append(titleLink);
-
-      const imgCell = document.createElement('td');
-      const img = document.createElement('img');
-      img.classList.add('gallery-image');
-      img.setAttribute('alt', object.title);
-      img.setAttribute('src', object.primaryimageurl);
-      imgCell.append(img);
-
-      const peopleCell = document.createElement('td');
-      if (object.people) {
-        const ul = document.createElement('ul');
-
-        // Build ul of people names
-        object.people.forEach((p) => {
-          const li = document.createElement('li');
-          li.append(p.name);
-          ul.append(li);
-        });
-
-        peopleCell.append(ul);
+    data.records.forEach((gallery) => {
+      if(floor == "all" || gallery.floor == floor) {
+      galleries.innerHTML += `
+      <li class="list-group-item">
+        <a href="#${gallery.id}" onclick="showObjectsTable(${gallery.id})">
+          Gallery #${gallery.id}: ${gallery.name} (Floor ${gallery.floor})
+        </a>
+      </li>
+      `;
       }
-
-      const extCell = document.createElement('td');
-      const extLink = document.createElement('a');
-      extLink.setAttribute('href', object.url);
-      extLink.append('See more');
-      extCell.append(extLink);
-
-      // Add cells to row
-      tr.append(titleCell, imgCell, peopleCell, extCell);
-
-      objectsTableBody.append(tr);
     });
 
     if (data.info.next) {
-      showObjects(data.info.next);
+      showGalleryFloor(data.info.next);
     }
   });
 }
 
+function clearHtml() {
+  galleries.innerHTML = "";
+}
 
-/**
- * Show information about object
- */
-function showObjectInfo(id) {
-  fetch(`https://api.harvardartmuseums.org/object/${id}?apikey=${API_KEY}`)
+function addFav(obj) {
+  let info = JSON.parse(window.localStorage.getItem('info'));
+  info.push(obj);
+  window.localStorage.setItem('info', JSON.stringify(info));
+}
+
+function showFavs(){
+  let info = JSON.parse(window.localStorage.getItem('info'));
+  if (info) {
+  info.favs.forEach((obj) => {
+    objectContainer.innerHTML += `
+      <tr>
+        <td>${obj.title}</td>
+        <td>${obj.description}</td>
+        <td>${obj.provenance}<\td>
+        <td>${obj.accessionyear}<\td>
+        <td>${obj.primaryimageurl}<\td>
+      </tr> `;
+  });
+}
+else {
+  objectContainer.innerHTML += `<h1>No Favorites<\h1>`
+}
+}
+
+function removeFav(obj) {
+  let info = JSON.parse(window.localStorage.getItem('info'));
+  for(var i = info.favs.length - 1; i >= 0; i--) {
+    if(info.favs[i].objectnumber === obj.objectnumber) {
+       array.splice(i, 1);
+    }
+  }
+  window.localStorage.setItem('info', JSON.stringify(info));
+  }
+
+function deleteAllFav() {
+  window.localStorage.clear();
+}
+
+function check(obj) {
+  let checkbox = document.querySelector(`#${obj.objectnumber}`);
+  if (checkbox.checked) {
+    addFav(obj);}
+  else {
+    removeFav(obj);
+  }
+  }
+
+function showObjectsTable(id) {
+  allObjects.style.display = "block";
+  allGalleries.style.display = "none";
+  objectview.style.display = "none";
+  fetch(`https://api.harvardartmuseums.org/object?apikey=${API_KEY}&gallery=${id}`)
   .then((response) => response.json())
-  .then((object) => {
-    objectContainer.innerHTML = '';
-    allObjects.style.display = 'none';
-    allGalleries.style.display = 'none';
-    objectDetails.style.display = 'block';
-
-    const ul = document.createElement('ul');
-    ['title', 'description', 'provenance', 'accessionyear'].forEach((key) => {
-      const li = document.createElement('li');
-      li.append(object[key] || 'None');
-      ul.append(li);
+  .then((data) => {
+    data.records.forEach((object) => {
+      objects.innerHTML += `
+      <tr>
+      <td><a href="#${object.objectnumber}" onclick="showObjectInfo('${object.objectnumber}');">${object.title}</a></td>
+        <td><img src=${object.primaryimageurl}></td>
+        <td>${object.people ? object.people.map(x => x.name): "Unknown"}</td>
+        <td><a href="${object.url}" target="_blank">Click to visit page</a></td>
+      </tr>
+    `;
     });
+    button1.innerHTML += `<input type="button" class = "btn btn-outline-success ml-3" value="Go Back" onclick="window.location.href='index.html'">`
+  });
+}
 
-    const img = document.createElement('img');
-    img.setAttribute('src', object.primaryimageurl);
-    img.setAttribute('alt', object.title);
-
-    const titleItem = document.createElement('li');
-    titleItem.append(object.title);
-    objectContainer.append(ul, img);
-
+function showObjectInfo(id) {
+  allObjects.style.display = "none";
+  allGalleries.style.display = "none";
+  objectview.style.display = "block";
+  let info = JSON.parse(window.localStorage.getItem('info'));
+  fetch(`https://api.harvardartmuseums.org/object?apikey=${API_KEY}&objectnumber=${id}`)
+  .then((response) => response.json())
+  .then((data) => {
+    data.records.forEach((obj) => {
+      let checkVar = (info && info.favs.indexOf(obj) != -1);
+      objectinfo.innerHTML += `
+      <tr>
+        <td>${obj.title}</td>
+        <td>${obj.description}</td>
+        <td>${obj.provenance}</td>
+        <td>${obj.accessionyear}</td>
+        <td><img src=${obj.primaryimageurl}></td>
+        <td><input type="checkbox" checked="${checkVar}" onclick="check(${obj})"></td>
+      </tr>
+    `;
+    });
+    button2.innerHTML += `<input type="button" class = "btn btn-outline-success ml-3" value="Go Back" onclick="window.location.href='index.html'">`
   });
 }
